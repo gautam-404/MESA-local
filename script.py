b9803a66f54a8f2a5570502ead4f6fd03c58bca0
gautam-404 <gautam.anuj.1195@gmail.com>
2023-01-18 06:11:59 +0530

    sort inlists

--- script.py
+++ script.py
@@ -1,12 +1,15 @@
 from MESAmanager import ProjectOps
 import glob
 
-inlists = glob.glob("./inlists/*_inlist_*")
+inlists = sorted(glob.glob("./inlists/*_inlist_*"))
 
 dsct = ProjectOps("dsct")       
 dsct.create(overwrite=True, clean=True)             
 dsct.make()
 
+
 for i in range(5):
+    print("Running inlist: ", inlists[i])
     dsct.loadProjInlist(inlists[i])
     dsct.run(silent=True)
+    
