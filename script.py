728a7aa2d90cd1110e7e3af5196ec155f5938b15
gautam-404 <gautam.anuj.1195@gmail.com>
2023-01-25 08:39:33 +0530

    update

--- script.py
+++ script.py
@@ -4,20 +4,17 @@ import glob
 inlists = sorted(glob.glob("./inlists/*_inlist_*"))
 
 proj = ProjectOps("dsct")       
-# proj.create(overwrite=True, clean=True)             
-# proj.make()
+proj.create(overwrite=False, clean=True)             
+proj.make()
 
-# star = MesaAccess("dsct2", astero=True)
+star = MesaAccess("dsct", astero=True)
+star.load_HistoryColumns("./inlists/history_columns.list")
+star.load_ProfileColumns("./inlists/profile_columns.list")
 
-# for i in range(5):
-#     print("Running for inlist: ", inlists[i])
-#     star.load_InlistProject(inlists[i])
-#     proj.run(silent=True)
+for i in range(5):
+    print("Running for inlist: ", inlists[i])
+    star.load_InlistProject(inlists[i])
+    proj.run(silent=True)
 
-# profs = sorted(glob.glob("./dsct/LOGS/*profile*.data.FGONG"))
 
-# for i in range():
-#     print("Running for inlist: ", inlists[i])
-#     proj.load_InlistProject(inlists[i])
-#     proj.run(silent=True)
 proj.runGyre("./inlists/gyre_template.in", silent=True)
