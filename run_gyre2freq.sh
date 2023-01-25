fbd7a44e160aa8bb143690ea460acb8280aa5fdf
gautam-404 <gautam.anuj.1195@gmail.com>
2023-01-25 06:58:55 +0530

    .

---
+++ run_gyre2freq.sh
@@ -0,0 +1,4 @@
+cd dsct/LOGS
+for FGONG in *.FGONG; do
+    /Users/anuj/Documents/MESA-workspace/MESA-local/gyre2freqs.sh -i $FGONG -f
+done
--- script.py
+++ script.py
@@ -1,14 +1,23 @@
-from MESAmanager import ProjectOps
+from MESAcontroller import ProjectOps, MesaAccess
 import glob
 
 inlists = sorted(glob.glob("./inlists/*_inlist_*"))
 
-dsct = ProjectOps("dsct")       
-dsct.create(overwrite=True, clean=True)             
-dsct.make()
+proj = ProjectOps("dsct")       
+# proj.create(overwrite=True, clean=True)             
+# proj.make()
 
+# star = MesaAccess("dsct2", astero=True)
 
-for i in range(5):
-    print("Running for inlist: ", inlists[i])
-    dsct.loadProjInlist(inlists[i])
-    dsct.run(silent=True)
+# for i in range(5):
+#     print("Running for inlist: ", inlists[i])
+#     star.load_InlistProject(inlists[i])
+#     proj.run(silent=True)
+
+# profs = sorted(glob.glob("./dsct/LOGS/*profile*.data.FGONG"))
+
+# for i in range():
+#     print("Running for inlist: ", inlists[i])
+#     proj.load_InlistProject(inlists[i])
+#     proj.run(silent=True)
+proj.runGyre("./inlists/gyre_template.in", silent=True)
