8181dbffebbce92c10dc2df9f4005c78a4d42693
gautam-404 <gautam.anuj.1195@gmail.com>
2023-02-08 08:52:54 +0530

    update

--- script.py
+++ script.py
@@ -15,8 +15,8 @@ star = MesaAccess("test")
 star.load_HistoryColumns("./inlists/history_columns.list")
 star.load_ProfileColumns("./inlists/profile_columns.list")
 
-Zinit = 0.0105
-initial_mass = 1.52
+Zinit = 0.02
+initial_mass = 2.0
 
 ## Initial Contraction
 print("Inlist: ", inlist_template)
@@ -29,7 +29,7 @@ input_params = {'initial_mass': initial_mass, 'initial_z': Zinit, 'Zbase': Zinit
                 'create_pre_main_sequence_model': True, 'pre_ms_T_c': 9e5,
                 'set_uniform_initial_composition' : True, 'initial_zfracs' : 6,
                 'max_model_number': 1}
-star.set(input_params)
+star.set(input_params, force=True)
 star.set('max_age', 1.0E-3)
 proj.run(silent=True)
 
@@ -44,7 +44,7 @@ input_params = {'initial_mass': initial_mass, 'initial_z': Zinit, 'Zbase': Zinit
                 'relax_mass' : True, 'lg_max_abs_mdot' : 6, 'new_mass' : initial_mass,
                 'max_model_number': -1, 
                 'write_header_frequency': 10, 'history_interval': 1, 'terminal_interval': 10, 'profile_interval': 50}
-star.set(input_params)
+star.set(input_params, force=True)
 star.set('max_age', 2.0E6)
 proj.resume(silent=True)
 
@@ -57,7 +57,7 @@ input_params = {'initial_mass': initial_mass, 'initial_z': Zinit, 'Zbase': Zinit
                 'save_model_when_terminate': True, 'save_model_filename': 'evolution.mod',
                 'max_model_number': -1,
                 'write_header_frequency': 10, 'history_interval': 15, 'terminal_interval': 15, 'profile_interval': 15}
-star.set(input_params)
+star.set(input_params, force=True)
 star.set('max_age', 2.0E7)
 proj.resume(silent=True)
 
@@ -70,7 +70,7 @@ input_params = {'initial_mass': initial_mass, 'initial_z': Zinit, 'Zbase': Zinit
                 'save_model_when_terminate': True, 'save_model_filename': 'evolution.mod',
                 'max_model_number': -1,
                 'write_header_frequency': 4, 'history_interval': 4, 'terminal_interval': 4, 'profile_interval': 4}
-star.set(input_params)
+star.set(input_params, force=True)
 star.set('max_age', 4.0E7)
 proj.resume(silent=True)
 
@@ -83,7 +83,7 @@ input_params = {'initial_mass': initial_mass, 'initial_z': Zinit, 'Zbase': Zinit
                 'save_model_when_terminate': True, 'save_model_filename': 'evolution.mod',
                 'max_model_number': -1,
                 'write_header_frequency': 1, 'history_interval': 1, 'terminal_interval': 1, 'profile_interval': 1}
-star.set(input_params)
+star.set(input_params, force=True)
 terminal_age = float(np.round(2500/initial_mass**2.5,1)*1.0E6)
 star.set('max_age', terminal_age)
 proj.resume(silent=True)
