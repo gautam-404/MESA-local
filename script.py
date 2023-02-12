c59b21cea52a0359fa050bc2d9933233ca1deefe
gautam-404 <gautam.anuj.1195@gmail.com>
2023-02-13 02:54:02 +0530

    rotation

--- script.py
+++ script.py
@@ -3,7 +3,7 @@ import numpy as np
 import helper
 
 
-def run_star(mass, metallicity, logging=False):
+def run_star(mass, metallicity, ZAMS_surface_v_rot=0, logging=False):
     inlist_template = "./inlists/inlist_template"
 
     name=f"test"
@@ -17,7 +17,7 @@ def run_star(mass, metallicity, logging=False):
     initial_mass = mass
     Zinit = metallicity
 
-    phases_params = helper.phases_params(initial_mass, Zinit)
+    phases_params = helper.phases_params_rotation(initial_mass, Zinit, ZAMS_surface_v_rot)
     terminal_age = float(np.round(2500/initial_mass**2.5,1)*1.0E6)
     phase_max_age = [1.0E-3, 2.0E6, 2.0E7, 4.0E7, terminal_age]
 
@@ -35,4 +35,4 @@ def run_star(mass, metallicity, logging=False):
     proj.runGyre(gyre_in="./inlists/gyre_template.in", files='all', logging=logging)
 
 
-run_star(1.6, 0.0065, logging=True)
+run_star(mass=1.6, metallicity=0.0065, ZAMS_surface_v_rot=1.8, logging=True)
