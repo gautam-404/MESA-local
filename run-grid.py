4c03f425250818a9cbed45c6738481780829b83b
Anuj Gautam <anujgautam11@gmail.com>
2023-02-12 07:38:11 +0000

    grid

--- run-grid.py
+++ run-grid.py
@@ -2,17 +2,7 @@ from MESAcontroller import ProjectOps, MesaAccess
 import glob
 import numpy as np
 import helper
-
-
-def run_phase(proj, star, inlist_template, input_params, phase_name, max_age, resume=False):
-    print(phase_name)
-    star.load_InlistProject(inlist_template)
-    star.set(input_params, force=True)
-    star.set('max_age', max_age)
-    if resume:
-        proj.resume(silent=True)
-    else:
-        proj.run(silent=True)
+import os, shutil
 
 def run_star(mass, metallicity):
     inlist_template = "./inlists/inlist_template"
@@ -40,11 +30,48 @@ def run_star(mass, metallicity):
             proj.run(silent=True)
         else:
             proj.resume(silent=True)
+            
+    ## Run GYRE
+    proj.runGyre(gyre_in="./inlists/gyre_template.in", files='all')
 
 
 ## Main script
 if __name__ == "__main__":
-    run_star(1.6, 0.0065)
+    testrun = False
+
+    if testrun:
+        run_star(1.6, 0.0065)
+    else:
+        arr = np.genfromtxt("coarse_age_map.csv",
+                        delimiter=",", dtype=str, skip_header=1)
+        masses = arr[:,0].astype(float)
+        metallicities = arr[:,1].astype(float)
+
+        ## Create archive directories
+        os.mkdir("grid_LOGSarchive")
+        os.mkdir("grid_LOGSarchive/histories")
+        os.mkdir("grid_LOGSarchive/profiles")
+        os.mkdir("grid_LOGSarchive/gyre")
+
+        ## Run grid
+        model = 1
+        for mass, metallicity in zip(masses, metallicities):
+            run_star(mass, metallicity)
+
+            ## Archive LOGS
+            os.mkdir(f"grid_LOGSarchive/gyre/model_{model}")
+            for file in glob.glob("test/LOGS/*"):
+                if "history_.data" in file:
+                    shutil.move(file, f"grid_LOGSarchive/histories/history_{model}.data")
+                elif "profile.index" in file:
+                    shutil.move(file, f"grid_LOGSarchive/profiles/profile_{model}.index")
+                elif "-freqs.dat" in file:
+                    shutil.copy(file, f"grid_LOGSarchive/gyre/model_{model}")
+            model += 1
+
+
+    
+    
 
     
 
