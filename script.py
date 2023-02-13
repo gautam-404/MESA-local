490e01984e12701b5493ebf185cab250431ad59e
gautam-404 <gautam.anuj.1195@gmail.com>
2023-02-13 12:01:33 +0530

    update

--- script.py
+++ script.py
@@ -1,11 +1,12 @@
-from MESAcontroller import ProjectOps, MesaAccess
+import glob
+
 import numpy as np
+from MESAcontroller import MesaAccess, ProjectOps
+
 import helper
 
 
-def run_star(mass, metallicity, ZAMS_surface_v_rot=0, logging=False):
-    inlist_template = "./inlists/inlist_template"
-
+def evo_star(mass, metallicity, ZAMS_surface_v_rot=0, logging=False, loadInlists=False):
     name=f"test"
     proj = ProjectOps(name)     
     proj.create(overwrite=True)             
@@ -19,20 +20,38 @@ def run_star(mass, metallicity, ZAMS_surface_v_rot=0, logging=False):
 
     phases_params = helper.phases_params_rotation(initial_mass, Zinit, ZAMS_surface_v_rot)
     terminal_age = float(np.round(2500/initial_mass**2.5,1)*1.0E6)
-    phase_max_age = [1.0E-3, 2.0E6, 2.0E7, 4.0E7, terminal_age]
+    phase_max_age = [1.0E-3, 1.0E6, 1.5E6, 2.0E7, 4.0E7, terminal_age]
 
-    for phase_name, input_params in phases_params.items():
-        print(phase_name)
-        star.load_InlistProject(inlist_template)
-        star.set(input_params, force=True)
-        star.set('max_age', phase_max_age.pop(0))
-        if phase_name == "Initial Contraction":
-            proj.run(logging=logging)
-        else:
-            proj.resume(logging=logging)
+    if loadInlists:
+        ## Run MESA from pre-made inlists
+        rotation_templates = sorted(glob.glob("./urot/*inlist*"))
+        for phase_name in helper.phases_params_rotation(initial_mass, Zinit, ZAMS_surface_v_rot).keys():
+            print(phase_name)
+            star.load_InlistProject(rotation_templates.pop(0))
+            star.set('initial_mass', initial_mass)
+            star.set('initial_z', Zinit)
+            if phase_name == "Start rotation":
+                star.set('new_surface_rotation_v', ZAMS_surface_v_rot)
+            star.set('max_age', phase_max_age.pop(0))
+            if phase_name == "Initial Contraction":
+                proj.run(logging=logging)
+            else:
+                proj.resume(logging=logging)
+    else:
+        ## Run MESA from inlist template by setting parameters for each phase
+        inlist_template = "./inlists/inlist_template"
+        for phase_name, input_params in phases_params.items():
+            print(phase_name)
+            star.load_InlistProject(inlist_template)
+            star.set(input_params, force=True)
+            star.set('max_age', phase_max_age.pop(0))
+            if phase_name == "Initial Contraction":
+                proj.run(logging=logging)
+            else:
+                proj.resume(logging=logging)
             
     ## Run GYRE
-    proj.runGyre(gyre_in="./inlists/gyre_template.in", files='all', logging=logging)
+    proj.runGyre(gyre_in="urot/gyre_rot_template_all_modes.in", data_format="GYRE", files='all', logging=logging)
 
 
-run_star(mass=1.6, metallicity=0.0065, ZAMS_surface_v_rot=1.8, logging=True)
+evo_star(mass=1.6, metallicity=0.0065, ZAMS_surface_v_rot=1.8, loadInlists=True, logging=True)
